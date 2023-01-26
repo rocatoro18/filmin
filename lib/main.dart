2bd77d9f5364feaa21e6ecdc6da6fdd406bc649b
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2023-01-26 10:31:46 -0700

    provider & http

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:filmin/screens/screens.dart';
+import 'package:provider/provider.dart';
+import 'package:filmin/providers/movies_provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        )
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
